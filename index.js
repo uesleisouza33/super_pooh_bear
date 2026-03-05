672a290a32bf389d1f121310f059204835ee6843
Ueslei Carvalho <ucarvalhof@gmail.com>
2026-03-05 07:52:42 -0300

    Versão 0.01 - Corrigido erros no html, criando versão base da class Pooh e index.js inicial

---
+++ index.js
@@ -0,0 +1,29 @@
+import Pooh from "./models/Pooh.js";
+
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+let keys = {};
+
+document.addEventListener("keydown", (e) => (keys[e.key] = true));
+document.addEventListener("keyup", (e) => (keys[e.key] = false));
+
+let poohInfo = { x: 50, y: 200, width: 50, height: 50, color: "yellow" };
+let pooh = new Pooh(poohInfo);
+
+function update() {
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = pooh.color;
+  ctx.fillRect(pooh.x, pooh.y, pooh.width, pooh.height);
+}
+
+function gameLoop() {
+  update();
+  draw();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
---
+++ models/Pooh.js
@@ -0,0 +1,10 @@
+export default class Pooh {
+  constructor(info) {
+    this.info = info;
+    this.x = info.x;
+    this.y = info.y;
+    this.width = info.width;
+    this.height = info.height;
+    this.color = info.color;
+  }
+}
